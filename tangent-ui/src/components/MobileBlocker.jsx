0f3a3c578ab673762ad67bbb6be98f456863c9f4
alternatelens <alternatelensfilms@gmail.com>
2025-04-29 23:32:37 +0100

    Initial commit to Dialogos

--- tangent-ui/src/components/MobileBlocker.jsx
+++ tangent-ui/src/components/MobileBlocker.jsx
@@ -20,18 +20,16 @@ export default function MobileBlocker() {
     <div
       className="
         fixed inset-0 z-[9999] flex items-center justify-center
-        bg-white/50 dark:bg-black/50
+        bg-black/47 dark:bg-black/50
         backdrop-blur-md
         p-6 text-center
       "
     >
-      <div className="max-w-sm bg-white/80 dark:bg-black/80 rounded-lg p-6 shadow-lg">
-        <h1 className="text-2xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+      <div className=" ">
+        <h1 className="text-2xl font-bold mb-2 text-white-900 dark:text-gray-100">
           Please run app on desktop
         </h1>
-        <p className="text-lg text-gray-700 dark:text-gray-200">
-          This application is only supported on desktop devices.
-        </p>
+       
       </div>
     </div>
   );
